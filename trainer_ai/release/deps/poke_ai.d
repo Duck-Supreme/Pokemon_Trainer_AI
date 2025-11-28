3c8aae444d14f9f2b3131172814bda9802844f77
marti <marti@DESKTOP-P7330OI>
2025-11-27 21:14:48 -0600

    it begins

--- trainer_ai/release/deps/poke_ai.d
+++
@@ -1,5 +0,0 @@
-C:\Users\marti\RustroverProjects\Pokemon_Trainer_AI\poke_ai\../trainer_ai\release\deps\poke_ai.d: src\main.rs
-
-C:\Users\marti\RustroverProjects\Pokemon_Trainer_AI\poke_ai\../trainer_ai\release\deps\poke_ai.exe: src\main.rs
-
-src\main.rs:
--- trainer_ai/release/poke_ai.d
+++
@@ -1,1 +0,0 @@
-C:\Users\marti\RustroverProjects\Pokemon_Trainer_AI\poke_ai\../trainer_ai\release\poke_ai.exe: C:\Users\marti\RustroverProjects\Pokemon_Trainer_AI\poke_ai\src\main.rs
